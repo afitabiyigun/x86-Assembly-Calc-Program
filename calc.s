7b430b9f8f1163be64bdad075a981ef1a51424bf
Afitab Iyigun <afitab@bu.edu>
2024-10-28 18:35:36 +0000

    removed variables SUM_POSITIVE, SUM_NEGATIVE, CALC_DATA_BEGIN since they are already globally defined in my xor.s and basic_cmds.s files

--- calc.s
+++ calc.s
@@ -1,12 +1,12 @@
 	.intel_syntax noprefix
 	.section .data
-	.global SUM_POSITIVE
-	.global SUM_NEGATIVE
-	.global CALC_DATA_BEGIN
 	.global _start
 
 _start:
 	xor rax, rax
-	xor rbx. rbx 
+	xor rbx, rbx
+	xor cl, cl
 	mov rbx, OFFSET [CALC_DATA_BEGIN]
-	
+	mov cl, BYTE PTR [CALC_DATA_BEGIN]
+
+
